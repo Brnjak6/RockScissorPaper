eedcc58b663cb99ef1e0e323e766a0e93f7cf19d
OPTIC <68736438+Brnjak6@users.noreply.github.com>
2022-03-23 18:55:51 +0000

    lab 10 upgrade

--- script.js
+++ script.js
@@ -4,9 +4,18 @@ const userDefeated = document.querySelector('.defeat');
 const drawResult = document.querySelector('.draw');
 const restartButton = document.querySelector('.sequel');
 const computerMessage = document.getElementById('computer_message');
+const triesLeft = document.getElementById('tries_left');
+const choicesArray = document.getElementById('choices_array');
+const choicesTitle = document.getElementById('choices_title');
+let tries = 5;
+const triesArray = [];
+triesLeft.innerHTML = tries;
 
 function mySelection(e) {
   const computerChoice = Math.floor(Math.random() * 3) + 1;
+  tries--;
+  triesLeft.innerHTML = tries;
+  triesArray.push(e);
 
   if (
     (e === 1 && computerChoice === 2) ||
@@ -27,6 +36,38 @@ function mySelection(e) {
     computerMessage.innerHTML = "Computer's choice was Paper";
   }
   restartButton.style.display = 'flex';
+
+  if (tries == 0) {
+    restartButton.style.display = 'none';
+    choiceDiv.style.display = 'none';
+
+    choicesTitle.style.display = 'inline';
+
+    for (let i = 0; i < triesArray.length; i++) {
+      if (triesArray[i] == 1) {
+        choicesArray.innerHTML += `<img
+        src="./svg/rock.svg"
+        width="75"
+        height="75"
+   
+      />`;
+      } else if (triesArray[i] == 2) {
+        choicesArray.innerHTML += `<img
+        src="./svg/scissor.svg"
+        width="75"
+        height="75"
+  
+      />`;
+      } else if (triesArray[i] == 3) {
+        choicesArray.innerHTML += `<img
+        src="./svg/paper.svg"
+        width="75"
+        height="75"
+   
+      />`;
+      }
+    }
+  }
 }
 
 const win = () => {
